f4181915315515daba9ea0c7d67f1d0b7d0937b7
TeemourA <taxprogramming@gmail.com>
2021-04-12 19:36:26 +0300

    change 8-day forecast layout

--- src/components/EightDaysData/EightDaysData.tsx
+++ src/components/EightDaysData/EightDaysData.tsx
@@ -18,17 +18,21 @@ const EightDaysData: React.FC<EightDaysDataProps> = props => {
       <ul className="eight-days-info__list">
         {data.map((day: any, index: number) => (
           <li key={index} className="eight-days-info__item">
-            <span>{day.date}</span>
-            <figure>
+            <span className="eight-days-info__title">{day.date}</span>
+            <figure className="eight-days-info__figure">
               <img
                 src={day.weatherIcon}
                 alt={day?.iconDescription}
                 title={day?.iconDescription}
+                className="eight-days-info__img"
               />
-              <figcaption>{day.iconDescription}</figcaption>
+              <figcaption className="eight-days-info__img_info">{day.iconDescription}</figcaption>
             </figure>
-            <span>{`Day: ${day.temp.max}`}</span>
-            <span>{`Night: ${day.temp.min}`}</span>
+            <div className="eight-days-info__temp">
+              <span className="eight-days-info__temp_max">{day.temp.max}</span>
+              {' / '}
+              <span className="eight-days-info__temp_min">{day.temp.min}</span>
+            </div>
           </li>
         ))}
       </ul>
